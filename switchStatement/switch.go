e022b0239c5882f3f1360492dce03fffe9c29b66
santanu4246 <santanu4246@gmail.com>
2026-07-26 16:17:44 +0530

    Refactor switch statement example to demonstrate type switching and remove commented-out code

--- switchStatement/switch.go
+++ switchStatement/switch.go
@@ -1,26 +1,52 @@
 package main
 
 import "fmt"
-
+// import "time"
 func main(){
 
-	i:=5
+	// var i int
 
-	switch i{
-	case 1:
+	// fmt.Println("Enter the number");
+	// fmt.Scan(&i)
+	// switch i{
+	// case 1:
 	 
-		fmt.Println("one")
+	// 	fmt.Println("one")
 	
-	case 2:
-		fmt.Println("two")
+	// case 2:
+	// 	fmt.Println("two")
 
-	case 3:
-		fmt.Println("three")
+	// case 3:
+	// 	fmt.Println("three")
 	
-	case 4:
-		fmt.Println("four")
+	// case 4:
+	// 	fmt.Println("four")
 
-	default:
-		fmt.Println("Default")
+	// default:
+	// 	fmt.Println("Default")
+	// }
+
+	// switch time.Now().Weekday(){
+
+	// case time.Saturday, time.Sunday:
+	// 	fmt.Println("Weekend")
+	//  default:
+	// 	fmt.Println("Weekday")
+	// }
+
+	// type switch
+
+	whoami := func(i interface{}){
+		switch t := i.(type){
+		case int:
+			fmt.Println("Integar")
+		case string:
+			fmt.Println("String")
+		case bool:
+			fmt.Println("Boolean")
+		default:
+			fmt.Println("Others", t)
+		}
 	}
+	whoami(6)
 }
