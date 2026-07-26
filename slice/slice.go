f053ede26628423e80bc738810e45612f43ee912
santanu4246 <santanu4246@gmail.com>
2026-07-26 18:33:18 +0530

    Comment out main function code and add additional comments for clarity on slice operations

--- slice/slice.go
+++ slice/slice.go
@@ -1,21 +1,35 @@
 package main
 
-import "fmt"
+// import "fmt"
 
-func main(){
+func main() {
 
-	var n int;
+	// var n int;
 
-	
-	fmt.Println("Enter the size:-");
-	fmt.Scan(&n);
+	// fmt.Println("Enter the size:-");
+	// fmt.Scan(&n);
 
-	numbers := make([]int,n)
-	
-	fmt.Println("Enter the elements:-");
-	for i := 0; i < n; i++{
-		fmt.Scan(&numbers[i]);
-	}
+	// numbers := make([]int,n)
 
-	fmt.Println(numbers)
+	// fmt.Println("Enter the elements:-");
+	// for i := 0; i < n; i++{
+	// 	fmt.Scan(&numbers[i]);
+	// }
+
+	// fmt.Println(numbers)
+
+	// loop over a slice
+	// numbers := []int{10, 20, 30, 40}
+
+	// for i, value := range numbers {
+	// 	fmt.Println(i, value)
+	// }
+
+	//delete eliment from index 2
+
+	// numbers := []int{10, 20, 30, 40, 50}
+
+	// numbers = append(numbers[:2], numbers[3:]...)
+
+	// fmt.Println(numbers)
 }
