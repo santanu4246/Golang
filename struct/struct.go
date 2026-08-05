2e9878a1775e7df2cb710d4c417821df77119622
santanu4246 <santanu4246@gmail.com>
2026-08-05 18:23:30 +0530

    Refactor User struct in struct.go by commenting out the Address field and adding a Greet method. Update main function to demonstrate user greetings and initialize a slice of users for improved functionality.

--- struct/struct.go
+++ struct/struct.go
@@ -3,9 +3,9 @@ package main
 import "fmt"
 
 type User struct {
-	Name    string
-	age     int
-	Address Address
+	Name string
+	age  int
+	// Address Address
 }
 
 // nested struct
@@ -14,17 +14,46 @@ type Address struct {
 	Country string
 }
 
+func (u User) Greet(){
+
+	fmt.Println("hello", u.Name)
+}
 func main() {
 
 	user := User{
 		Name: "Santanu",
 		age:  22,
-		Address: Address{
-			City: "bankura",
-			Country: "India",
-		},
+		// Address: Address{
+		// 	City: "bankura",
+		// 	Country: "India",
+		// },
 	}
 
 	fmt.Println(user)
+
+	users := []User{
+		{
+			Name: "Santanu",
+			age:  21,
+		},
+
+		{
+			Name: "chudShubh",
+			age:  26,
+		},
+
+		{
+			Name: "roy",
+			age:  22,
+		},
+	}
+
+	fmt.Println(users)
+
+	for _, user := range users{
+		user.Greet()
+	}
+
+
 
 }
