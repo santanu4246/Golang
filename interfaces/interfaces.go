8dfeac4182b5604486fa99020c3ff6a8ed6e3eae
santanu4246 <santanu4246@gmail.com>
2026-08-11 22:38:50 +0530

    Enhance payment functionality by adding refund method to paymenter interface and implementing it in razorpay and stripe structs. Update main function to demonstrate payment and refund processes for improved payment handling.

--- interfaces/interfaces.go
+++ interfaces/interfaces.go
@@ -2,49 +2,64 @@ package main
 
 import "fmt"
 
-type paymenter interface{
-
+type paymenter interface {
 	pay(amount float32)
+	refund(amount float32, account string)
 }
 
-type payment struct{
-
+type payment struct {
 	gateway paymenter
 }
 
-func (p payment) makePayment(amount float32){
+func (p payment) makePayment(amount float32) {
 
 	// razorpayPaymentGw := razorpay{}
 	// razorpayPaymentGw.pay(amount)
 
 	p.gateway.pay(amount)
-	
+
+}
+
+func (p payment) getRefund(amount float32, account string) {
+
+	p.gateway.refund(amount, account)
 }
 
 type razorpay struct{}
 
-func (r razorpay) pay(amount float32){
+func (r razorpay) pay(amount float32) {
 
 	fmt.Println("making payment using razorpay", amount)
 }
 
+func (s razorpay) refund(amount float32, account string) {
+
+	fmt.Println("refunding using stripe", amount, account)
+
+}
+
 type stripe struct{}
 
-func (s stripe) pay(amount float32){
+func (s stripe) pay(amount float32) {
 
 	fmt.Println("makeing payment using stripe", amount)
 }
 
-func main(){
+func (s stripe) refund(amount float32, account string) {
+
+	fmt.Println("refunding using stripe", amount, account)
+
+}
+
+func main() {
 
 	// razorpayPaymentGw := razorpay{}
-	stripeGw:= stripe{}
+	stripeGw := stripe{}
 
 	newPayment := payment{
 		gateway: stripeGw,
 	}
 
 	newPayment.makePayment(100)
-
+	newPayment.getRefund(50, "account123")
 }
-
